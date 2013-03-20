a30c0e7ff9685b89bf143dc267603d225136e69c
Rodrigo Navarro <rnavarro1@gmail.com>
2013-03-20 13:28:57 -0300

    Documenting the opcodes.

--- src/vm.js
+++ src/vm.js
@@ -108,69 +108,129 @@
       switch (opcode & 0xF000) {
         case 0x000:
           switch(opcode) {
+            // 00E0 - CLS
+            // Clear the display.
             case 0x00E0:
               this.screen.clear();
               break;
 
+            // 00EE - RET
+            // Return from a subroutine.
+            // The interpreter sets the program counter to the address at the
+            //   top of the stack, then subtracts 1 from the stack pointer.
             case 0x00EE:
               this.pc = this.stack.pop();
               break;
           }
           break;
 
+        // 1nnn - JP addr
+        // Jump to location nnn.
+        // The interpreter sets the program counter to nnn.
         case 0x1000:
           this.pc = opcode & 0x0FFF;
           break;
 
+        // 2nnn - CALL addr
+        // Call subroutine at nnn.
+        // The interpreter increments the stack pointer, then puts the current
+        //   PC on the top of the stack. The PC is then set to nnn.
         case 0x2000:
           this.stack.push(this.pc);
           this.pc = opcode & 0x0FFF;
           break;
 
+        // 3xkk - SE Vx, byte
+        // Skip next instruction if Vx = kk.
+        // The interpreter compares register Vx to kk, and if they are equal,
+        //   increments the program counter by 2.
         case 0x3000:
           if (this.v[x] == (opcode & 0x00FF)) {
             this.pc += 2;
           }
           break;
 
+        // 4xkk - SNE Vx, byte
+        // Skip next instruction if Vx != kk.
+        // The interpreter compares register Vx to kk, and if they are not
+        //   equal, increments the program counter by 2.
         case 0x4000:
           if (this.v[x] != (opcode & 0x00FF)) {
             this.pc += 2;
           }
           break;
 
+        // 5xy0 - SE Vx, Vy
+        // Skip next instruction if Vx = Vy.
+        // The interpreter compares register Vx to register Vy, and if they are
+        //   equal, increments the program counter by 2.
         case 0x5000:
           if (this.v[x] == this.v[y]) {
             this.pc += 2;
           }
           break;
 
+        // 6xkk - LD Vx, byte
+        // Set Vx = kk.
+        // The interpreter puts the value kk into register Vx.
         case 0x6000:
           this.v[x] = opcode & 0x00FF;
           break;
 
+        // 7xkk - ADD Vx, byte
+        // Set Vx = Vx + kk.
+        // Adds the value kk to the value of register Vx, then stores the
+        //   result in Vx.
         case 0x7000:
           this.v[x] += opcode & 0x00FF;
           break;
 
         case 0x8000:
           switch (opcode & 0x000F) {
+            // 8xy0 - LD Vx, Vy
+            // Set Vx = Vy.
+            // Stores the value of register Vy in register Vx.
             case 0x0000:
               this.v[x] = this.v[y];
               break;
 
+            // 8xy1 - OR Vx, Vy
+            // Set Vx = Vx OR Vy.
+            // Performs a bitwise OR on the values of Vx and Vy, then stores the
+            //   result in Vx. A bitwise OR compares the corrseponding
+            //   bits from two values, and if either bit is 1, then the same bit
+            //   in the result is also 1. Otherwise, it is 0.
             case 0x0001:
               this.v[x] = this.v[x] | this.v[y];
               break;
 
+            // 8xy2 - AND Vx, Vy
+            // Set Vx = Vx AND Vy.
+            // Performs a bitwise AND on the values of Vx and Vy, then stores
+            //   the result in Vx. A bitwise AND compares the corrseponding
+            //   bits from two values, and if both bits are 1, then the same
+            //   bit in the result is also 1. Otherwise, it is 0.
             case 0x0002:
               this.v[x] = this.v[x] & this.v[y];
               break;
 
+            // 8xy3 - XOR Vx, Vy
+            // Set Vx = Vx XOR Vy.
+            // Performs a bitwise exclusive OR on the values of Vx and Vy, then
+            //   stores the result in Vx. An exclusive OR compares the
+            //   corrseponding bits from two values, and if the bits are not
+            //   both the same, then the corresponding bit in the result is set
+            //   to 1. Otherwise, it is 0.
             case 0x0003:
               this.v[x] = this.v[x] ^ this.v[y];
               break;
 
+            // 8xy4 - ADD Vx, Vy
+            // Set Vx = Vx + Vy, set VF = carry.
+            // The values of Vx and Vy are added together. If the result is
+            //   greater than 8 bits (i.e., > 255,) VF is set to 1,
+            //   otherwise 0. Only the lowest 8 bits of the result are kept,
+            //   and stored in Vx.
             case 0x0004:
               var sum = this.v[x] + this.v[y];
 
@@ -183,6 +243,10 @@
               this.v[x] = sum;
               break;
 
+            // 8xy5 - SUB Vx, Vy
+            // Set Vx = Vx - Vy, set VF = NOT borrow.
+            // If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is
+            //   subtracted from Vx, and the results stored in Vx.
             case 0x0005:
               if(this.v[x] > this.v[y]) {
                 this.v[0xF] = 1;
@@ -193,11 +257,19 @@
               this.v[x] = this.v[x] - this.v[y];
               break;
 
+            // 8xy6 - SHR Vx {, Vy}
+            // Set Vx = Vx SHR 1.
+            // If the least-significant bit of Vx is 1, then VF is set to 1,
+            //   otherwise 0. Then Vx is divided by 2.
             case 0x0006:
               this.v[0xF] = this.v[x] & 0x01;
               this.v[x] = this.v[x] >> 1;
               break;
 
+            // 8xy7 - SUBN Vx, Vy
+            // Set Vx = Vy - Vx, set VF = NOT borrow.
+            // If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is
+            //   subtracted from Vy, and the results stored in Vx.
             case 0x0007:
               if (this.v[x] > this.v[y]) {
                 this.v[0xF] = 0;
@@ -208,6 +280,10 @@
               this.v[x] = this.v[y] - this.v[x];
               break;
 
+            // 8xyE - SHL Vx {, Vy}
+            // Set Vx = Vx SHL 1.
+            // If the most-significant bit of Vx is 1, then VF is set to 1,
+            //   otherwise to 0. Then Vx is multiplied by 2.
             case 0x000E:
               this.v[0xF] = this.v[x] & 0x80;
               this.v[x] = this.v[x] << 1;
@@ -215,22 +291,40 @@
           }
           break;
 
+        // 9xy0 - SNE Vx, Vy
+        // Skip next instruction if Vx != Vy.
+        // The values of Vx and Vy are compared, and if they are not equal, the
+        //   program counter is increased by 2.
         case 0x9000:
           if (this.v[x] != this.v[y]) this.pc += 2;
           break;
 
+        // Annn - LD I, addr
+        // Set I = nnn.
+        // The value of register I is set to nnn.
         case 0xA000:
           this.i = opcode & 0x0FFF;
           break;
 
+        // Bnnn - JP V0, addr
+        // Jump to location nnn + V0.
+        // The program counter is set to nnn plus the value of V0.
         case 0xB000:
           this.pc = (opcode & 0x0FFF) + this.v[0];
           break;
 
+        // Cxkk - RND Vx, byte
+        // Set Vx = random byte AND kk.
+        // The interpreter generates a random number from 0 to 255, which is
+        //   then ANDed with the value kk. The results are stored in Vx. See
+        //   instruction 8xy2 for more information on AND.
         case 0xC000:
           this.v[x] = Math.floor(Math.random() * 0xFF) & (opcode & 0x00FF);
           break;
 
+        // Dxyn - DRW Vx, Vy, nibble
+        // Display n-byte sprite starting at memory location I at (Vx, Vy),
+        //   set VF = collision.
         case 0xD000:
           var row, col, sprite
             , width = 8
@@ -256,12 +350,16 @@
 
         case 0xE000:
           switch (opcode & 0x00FF) {
+            // Ex9E - SKP Vx
+            // Skip next instruction if key with the value of Vx is pressed.
             case 0x009E:
               if (this.input.isKeyPressed(this.v[x])) {
                 this.pc += 2;
               }
               break;
 
+            // ExA1 - SKNP Vx
+            // Skip next instruction if key with the value of Vx is not pressed.
             case 0x00A1:
               if (!this.input.isKeyPressed(this.v[x])) {
                 this.pc += 2;
@@ -272,10 +370,14 @@
 
         case 0xF000:
           switch (opcode & 0x00FF) {
+            // Fx07 - LD Vx, DT
+            // Set Vx = delay timer value.
             case 0x0007:
               this.v[x] = this.delayTimer;
               break;
 
+            // Fx0A - LD Vx, K
+            // Wait for a key press, store the value of the key in Vx.
             case 0x000A:
               this.paused = true;
 
@@ -285,36 +387,51 @@
                 this.input.onKeyPress = function() {}
               }.bind(this);
 
+            // Fx15 - LD DT, Vx
+            // Set delay timer = Vx.
             case 0x0015:
               this.delayTimer = this.v[x];
               break;
 
+            // Fx18 - LD ST, Vx
+            // Set sound timer = Vx.
             case 0x0018:
               this.soundTimer = this.v[x];
               break;
 
+            // Fx29 - LD F, Vx
+            // Set I = location of sprite for digit Vx.
             case 0x0029:
               this.i = this.v[x] * 5;
               break;
 
+            // Fx33 - LD B, Vx
+            // Store BCD representation of Vx in memory locations I,
+            //   I+1, and I+2.
             case 0x0033:
               this.memory[this.i]     = parseInt(this.v[x] / 100);
               this.memory[this.i + 1] = parseInt(this.v[x] % 100 / 10);
               this.memory[this.i + 2] = this.v[x] % 10;
               break;
 
+            // Fx55 - LD [I], Vx
+            // Store registers V0 through Vx in memory starting at location I.
             case 0x0055:
               for (var i = 0; i <= x; i++) {
                 this.memory[this.i + i] = this.v[i];
               }
               break;
 
+            // Fx65 - LD Vx, [I]
+            // Read registers V0 through Vx from memory starting at location I.
             case 0x0065:
               for (var i = 0; i <= x; i++) {
                 this.v[i] = this.memory[this.i + i];
               }
               break;
 
+            // Fx1E - ADD I, Vx
+            // Set I = I + Vx.
             case 0x001E:
               this.i += this.v[x];
               break;
